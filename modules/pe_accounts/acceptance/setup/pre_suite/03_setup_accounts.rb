19391f2598a131292e2648e617a074c866b40d24
reidmv <reid@puppetlabs.com>
2013-04-05 16:11:32 +0000

    Abort attempt to vendor pe_* modules

--- modules/pe_accounts/acceptance/setup/pre_suite/03_setup_accounts.rb
+++
@@ -1,5 +0,0 @@
-
-step "Removing accounts and groups that are used in the pe_accounts tests"
-
-master.user_absent('arthur')
-master.group_absent('arthur')
--- modules/pe_accounts/tests/init_yaml.pp
+++
@@ -1,9 +0,0 @@
-node default {
-  notify { 'alpha': }
-  ->
-  class  { 'pe_accounts':
-    data_store => 'yaml',
-  }
-  ->
-  notify { 'omega': }
-}
